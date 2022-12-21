a489cb1a3b47f601e1f95d604350bf9fbbcabe78
Sheerin Banu <sbanu@allurion.com>
2022-12-21 16:48:17 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static int num=188;
+
+    public static void main(String[] args){
+
+        System.out.println("Hello world");
+        System.out.println(num);
+
+
+    }
+}
